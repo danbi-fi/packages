b2aba4681ef4ca2867198710665e0e25d1894b2a
Eunchurn Park <ec.park@danbicorp.com>
2021-06-11 21:55:57 +0900

    chore(eslint): fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,6 @@
 export default {
   parser: "@typescript-eslint/parser", // Specifies ESLint parser
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    "prettier/@typescript-eslint",
-  ],
+  extends: ["plugin:@typescript-eslint/recommended", "plugin:prettier/recommended", "prettier/@typescript-eslint"],
   plugins: ["@typescript-eslint", "prettier"],
   parserOptions: {
     ecmaVersion: 2019, // Allows parsing of modern ECMA features
