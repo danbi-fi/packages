489cac2a475e763611d14054920d9072a1f2b23c
Eunchurn Park <ec.park@danbicorp.com>
2022-02-27 17:21:15 +0900

    fix(eslint): excape

--- packages/eslint-config/lib/index.js
+++ packages/eslint-config/lib/index.js
@@ -12,6 +12,20 @@ module.exports = {
         ecmaVersion: 2018,
         createDefaultProgram: true,
     },
-    rules: {},
+    rules: {
+        "prettier/prettier": ["error"],
+        "no-console": ["off"],
+        "import/no-extraneous-dependencies": ["off"],
+        "no-return-await": ["off"],
+        "import/extensions": ["off"],
+        "import/no-unresolved": ["off"],
+        "class-methods-use-this": ["off"],
+        "no-unused-vars": ["off"],
+        "no-useless-escape": ["off"],
+        "import/prefer-default-export": ["off"],
+        "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/no-explicit-any": ["warn"],
+    },
 };
 //# sourceMappingURL=index.js.map
